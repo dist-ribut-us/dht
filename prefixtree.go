2916ae3642d06d848045aa1282e6bee056ef9291
Adam Colton <adamcolton@gmail.com>
2019-06-03 23:01:00 -0400

    add a prefix tree to replace lists for efficient seek

---
+++ prefixtree.go
@@ -0,0 +1,206 @@
+package dht
+
+import (
+	"fmt"
+)
+
+var _ = fmt.Println
+
+type prefixBranch struct {
+	descendants uint
+	allowed     uint
+	toPrune     uint
+	branches    [2]*prefixBranch
+	val         NodeID
+}
+
+func (p *prefixBranch) insert(id NodeID, depth uint) uint {
+	if p.descendants == 0 {
+		p.val = id
+		p.descendants = 1
+		return 0
+	}
+	var valBit byte = 2
+	if p.val != nil {
+		if p.val.Equal(id) {
+			return 0
+		}
+		valBit = p.val.Bit(depth)
+		p.get(valBit).insert(p.val, depth+1)
+		p.val = nil
+	}
+	bit := id.Bit(depth)
+	p.toPrune = p.get(bit).insert(id, depth+1)
+	bit ^= 1
+	if p.branches[bit] != nil && p.toPrune < p.branches[bit].toPrune {
+		p.toPrune = p.branches[bit].toPrune
+	}
+
+	p.descendants++
+	if p.allowed > 0 && p.descendants > p.allowed {
+		if toPrune := p.descendants - p.allowed; toPrune > p.toPrune {
+			p.toPrune = toPrune
+		}
+	}
+
+	return p.toPrune
+}
+
+func (p *prefixBranch) get(idx byte) *prefixBranch {
+	branch := p.branches[idx]
+	if branch == nil {
+		branch = &prefixBranch{}
+		p.branches[idx] = branch
+	}
+	return branch
+}
+
+func (p *prefixBranch) search(target NodeID, depth uint) NodeID {
+	if p.val != nil {
+		return p.val
+	}
+	bit := target.Bit(depth)
+	if p.branches[bit] != nil {
+		return p.branches[bit].search(target, depth+1)
+	}
+	return p.branches[bit^1].search(target, depth+1)
+}
+
+func (p *prefixBranch) searchn(target NodeID, ids []NodeID, closerThan NodeID, depth uint) int {
+	if p.val != nil {
+		if closerThan == nil || p.val.Xor(target).Compare(closerThan) == -1 {
+			ids[0] = p.val
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	bit := target.Bit(depth)
+	var filled int
+	if p.branches[bit] != nil {
+		filled = p.branches[bit].searchn(target, ids, closerThan, depth+1)
+	}
+	if filled == len(ids) {
+		return filled
+	}
+	if p.branches[bit^1] != nil {
+		filled += p.branches[bit^1].searchn(target, ids[filled:], closerThan, depth+1)
+	}
+	return filled
+}
+
+func (p *prefixBranch) setAllowed(target NodeID, atDepth, allowed, depth uint) {
+	if depth == atDepth {
+		p.allowed = allowed
+		return
+	}
+	p.get(target.Bit(depth)).setAllowed(target, atDepth, allowed, depth+1)
+}
+
+//bool indicates if it's safe to remove this branch after pruning
+func (p *prefixBranch) prune(n uint, seenAllowed bool) bool {
+	canRemove := p.allowed == 0
+	if p.allowed > 0 {
+		seenAllowed = true
+	}
+
+	if p.val != nil {
+		if p.descendants != 1 {
+			panic("oh boy")
+		}
+		if n > 0 {
+			if n != 1 {
+				panic("really bad")
+			}
+			p.val = nil
+			p.descendants = 0
+		}
+		return canRemove && p.descendants == 0
+	}
+	if p.allowed > 0 && p.toPrune > n {
+		n = p.toPrune
+		p.toPrune = 0
+	}
+
+	var r uint
+	p.descendants = 0
+	if p.branches[1] != nil {
+		if p.branches[1].descendants < n {
+			r = n - p.branches[1].descendants
+		}
+		if p.branches[1].prune(n-r, seenAllowed) {
+			p.branches[1] = nil
+		} else {
+			p.descendants = p.branches[1].descendants
+			canRemove = false
+		}
+	} else {
+		r = n
+	}
+
+	if r > 0 && p.branches[0] == nil {
+		panic("something bad")
+	}
+
+	if p.branches[0] != nil {
+		if p.branches[0].prune(r, seenAllowed) {
+			p.branches[0] = nil
+		} else {
+			canRemove = false
+		}
+	}
+
+	if p.branches[0] != nil {
+		p.descendants += p.branches[0].descendants
+	}
+
+	return canRemove && p.descendants == 0
+}
+
+type tree struct {
+	root    *prefixBranch
+	id      NodeID
+	toPrune uint
+}
+
+func newTree(id NodeID, startBuffers int) *tree {
+	t := &tree{
+		root: &prefixBranch{},
+		id:   id,
+	}
+
+	allowed := uint(startBuffers)
+	ln := uint(len(id))*8 - 1
+	var i uint
+	for ; allowed > 1 && i < ln; i, allowed = i+1, allowed/2 {
+		t.root.setAllowed(id.FlipBit(int(i)), i, allowed, 0)
+	}
+	for ; i < ln; i++ {
+		t.root.setAllowed(id.FlipBit(int(i)), i, 1, 0)
+	}
+
+	return t
+}
+
+func (t *tree) insert(id NodeID) {
+	t.toPrune = t.root.insert(id, 0)
+}
+
+func (t *tree) search(id NodeID) NodeID {
+	return t.root.search(id, 0)
+}
+
+func (t *tree) searchn(id NodeID, n int, closerThan NodeID) []NodeID {
+	ids := make([]NodeID, n)
+	filled := t.root.searchn(id, ids, closerThan, 0)
+	return ids[:filled]
+}
+
+func (t *tree) prune() {
+	t.root.prune(0, false)
+}
+
+func (t *tree) Len() int {
+	return int(t.root.descendants) - 1
+}
---
+++ prefixtree_test.go
@@ -0,0 +1,103 @@
+package dht
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTree(t *testing.T) {
+	tr := newTree(randID(3), 4)
+	ids := []NodeID{
+		{10, 20, 30},
+		{100, 20, 30},
+		{10, 200, 111},
+		{100, 200, 111},
+	}
+
+	tr.insert(ids[0])
+	assert.Equal(t, ids[0], tr.search(ids[0]))
+
+	tr.insert(ids[1])
+	assert.Equal(t, ids[0], tr.search(ids[0]))
+	assert.Equal(t, ids[1], tr.search(ids[1]))
+	assert.Equal(t, ids[0], tr.search(ids[2]))
+	assert.Equal(t, ids[1], tr.search(ids[3]))
+
+	s := tr.searchn(ids[0], 5, nil)
+	assert.Len(t, s, 2)
+	assert.Equal(t, ids[0], s[0])
+	assert.Equal(t, ids[1], s[1])
+
+	s = tr.searchn(ids[1], 5, nil)
+	assert.Len(t, s, 2)
+	assert.Equal(t, ids[1], s[0])
+	assert.Equal(t, ids[0], s[1])
+
+	s = tr.searchn(ids[1], 5, NodeID{36, 12, 34})
+	assert.Len(t, s, 1)
+	assert.Equal(t, ids[1], s[0])
+
+	s = tr.searchn(ids[1], 5, NodeID{110, 0, 1})
+	assert.Len(t, s, 2)
+	assert.Equal(t, ids[1], s[0])
+	assert.Equal(t, ids[0], s[1])
+}
+
+func TestToPrune(t *testing.T) {
+	tr := newTree(NodeID{64, 0, 0}, 32)
+	tr.root.setAllowed(NodeID{128 + 64, 0, 0}, 2, 3, 0)
+
+	for i := byte(1); i < 10; i++ {
+		tr.insert(NodeID{128 + 64, i, 0})
+		if i < 4 {
+			assert.EqualValues(t, 0, tr.toPrune)
+		} else {
+			assert.EqualValues(t, i-3, tr.toPrune)
+		}
+	}
+	tr.insert(NodeID{32, 10, 20})
+	assert.EqualValues(t, 6, tr.toPrune)
+	tr.insert(NodeID{128, 10, 20})
+	assert.EqualValues(t, 6, tr.toPrune)
+	tr.insert(NodeID{64, 10, 20})
+	assert.EqualValues(t, 6, tr.toPrune)
+
+	tr.root.prune(0, false)
+}
+
+func TestFuzzTree(t *testing.T) {
+	ln := 10
+	for i := 0; i < 1; i++ {
+		l := NewList(randID(ln), -1)
+		tr := newTree(l.target, 64)
+
+		for j := 0; j < FuzzLoops; j++ {
+			id := randID(ln)
+			tr.insert(id)
+			l.AddNodeID(id)
+			assert.Equal(t, j, tr.Len())
+		}
+		ids := tr.searchn(l.target, FuzzLoops/10, nil)
+		assert.Len(t, ids, FuzzLoops/10)
+		for j, id := range ids {
+			assert.Equal(t, l.nodeIDs[j], id)
+		}
+		assert.Equal(t, ids, tr.searchn(l.target, FuzzLoops/5, l.diffs[FuzzLoops/10]))
+
+		tr.prune()
+		tr.root.checkAllowed()
+	}
+}
+
+func (p *prefixBranch) checkAllowed() {
+	if p.allowed > 0 && p.descendants > p.allowed {
+		panic("too many descendants")
+	}
+
+	if p.branches[0] != nil {
+		p.branches[0].checkAllowed()
+	}
+	if p.branches[1] != nil {
+		p.branches[1].checkAllowed()
+	}
+}
